480fd36330a5b7c8a5f33f8864144fcdb0d1a191
pantao1227 <pt27@live.cn>
2019-12-03 22:36:53 +0800

    update12003

--- pTest.c
+++ pTest.c
@@ -1,10 +1,12 @@
 #include<stdio.h>
 #include "matrix_op.h"
 
-matrix *createMat(int nRows, int nCols){
-    matrix *Mat = (matrix *)malloc(sizeof(int)*2+sizeof(double)*nRows*nCols);
+typedef matrix* mat_p;
 
-    return Mat;
+mat_p createMat(int nRows, int nCols){
+    mat_p mat = (matrix *)malloc(sizeof(int)*2+sizeof(double)*nRows*nCols);
+
+    return mat;
 }
 
 int main(){
